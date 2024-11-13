42facaf5269395fc3ec1d793e699a18cb152b209
likedrfit <809038249@qq.com>
2024-11-14 00:40:53 +0800

    [ADD]-新文件添加

---
+++ py_at_first_time/2_more_print.py
@@ -0,0 +1,5 @@
+print("hello" + "cnm" + "123")
+print("你好，我是\"helios\"")
+print("""第一行
+第二行
+第三行""")
---
+++ py_at_first_time/3_var_demo.py
@@ -0,0 +1,5 @@
+my_ex = "1234566777"
+my_darling = "5424234111"
+
+print("ex: " + my_ex)
+print("darling: " + my_darling)
